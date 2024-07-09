3831bde47fa0457d2437590b2d219d9e7294deaa
Ian Chow <chowi@arietid.physics.localnet>
2024-07-09 15:40:00 -0400

    refactored a bunch of files and re generated the processed comet and unnumbered minor planet files, still need to re generate the processed numbered planet files on a system with more ram

--- dynamics/mpc_process_cmt.py
+++ dynamics/mpc_process_cmt.py
@@ -7,19 +7,19 @@ n_lines = 425059248  # counted using sed -n '$=' NumObs.txt
 
 # Define a function to be applied to each chunk
 def process_chunk(chunk):
-    chunk.columns = ['packed_desig', 'year', 'obs']
+    chunk.columns = ['number', 'provisional_desig', 'year', 'obs']
     print(chunk)
     # get all rows where the last three digits are either G96 or 703 (the CSS observatories) and between 2005 and 2012 inclusive
     processed_chunk = chunk[((chunk.obs.astype('string').str[-3:] == 'G96') | (chunk.obs.astype('string').str[-3:] == '703')) &
     ((chunk.year.astype('string').str[-4:].astype('int') >= 2005) & (chunk.year.astype('string').str[-4:].astype('int') <= 2012))]
     # write to csv
-    processed_chunk.to_csv('mpc_data_unn/mpc_processed_unn_' + str(i) + '.csv')
+    processed_chunk.to_csv('mpc_data_cmt/mpc_processed_cmt_' + str(i) + '.csv')
     return None
 
 # Read the large DataFrame in chunks
-colspecs = [(0, 5), (5, 12), (12, 13), (13, 14), (14, 15), (15, 19), (32, 44), (44, 56), (56, 65), (65, 71), (71, 77), (77, 80)] 
+colspecs = [(0, 4), (4, 5), (5, 12), (12, 13), (13, 14), (14, 15), (15, 19), (32, 44), (44, 56), (56, 65), (65, 71), (71, 77), (77, 80)] 
 
-chunks = pd.read_fwf('UnnObs.txt', chunksize=chunksize, sep=None, header=None, colspecs=colspecs, usecols=[1, 5, 11], quoting=3, on_bad_lines='skip')
+chunks = pd.read_fwf('CmtObs.txt', chunksize=chunksize, sep=None, header=None, colspecs=colspecs, usecols=[0, 2, 6, 12], quoting=3, on_bad_lines='skip')
 
 for i, chunk in tqdm(enumerate(chunks)):  # approximate total number of iterations
     process_chunk(chunk)  # process chunk
--- dynamics/mpc_process_num.py
+++ dynamics/mpc_process_num.py
@@ -7,17 +7,19 @@ n_lines = 425059248  # counted using sed -n '$=' NumObs.txt
 
 # Define a function to be applied to each chunk
 def process_chunk(chunk):
-    chunk.columns = ['packed_desig', 'year', 'obs']
+    chunk.columns = ['packed_number', 'year', 'obs']
+    print(chunk)
     # get all rows where the last three digits are either G96 or 703 (the CSS observatories) and between 2005 and 2012 inclusive
     processed_chunk = chunk[((chunk.obs.astype('string').str[-3:] == 'G96') | (chunk.obs.astype('string').str[-3:] == '703')) &
     ((chunk.year.astype('string').str[-4:].astype('int') >= 2005) & (chunk.year.astype('string').str[-4:].astype('int') <= 2012))]
     # write to csv
-    processed_chunk.to_csv('mpc_data/mpc_processed_' + str(i) + '.csv')
-    print(processed_chunk.packed_desig.value_counts())
+    processed_chunk.to_csv('mpc_data_num/mpc_processed_num_' + str(i) + '.csv')
     return None
 
 # Read the large DataFrame in chunks
-chunks = pd.read_fwf('NumObs.txt', chunksize=chunksize, sep=None, header=None, usecols=[0, 1, 10], quoting=3, on_bad_lines='skip')
+colspecs = [(0, 5), (5, 12), (12, 13), (13, 14), (14, 15), (15, 19), (32, 44), (44, 56), (56, 65), (65, 71), (71, 77), (77, 80)] 
+
+chunks = pd.read_fwf('NumObs.txt', chunksize=chunksize, sep=None, header=None, colspecs=colspecs, usecols=[0, 5, 11], quoting=3, on_bad_lines='skip')
 
 for i, chunk in tqdm(enumerate(chunks)):  # approximate total number of iterations
     process_chunk(chunk)  # process chunk
