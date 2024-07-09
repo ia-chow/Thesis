ce3e7e3b547530ae154b501223d4b0076c7d95ae
ia-chow <ichow9@uwo.ca>
2024-07-09 00:12:43 -0400

    finally got the CSS stuff working... 2412 NEOs is less than what granvik reports (3632) but it's better than before

--- dynamics/mpc_process.py
+++ dynamics/mpc_process.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import multiprocessing
 from tqdm import tqdm
 
 chunksize = 10 ** 6  # chunksize for reading in pandas
@@ -9,19 +8,16 @@ n_lines = 425059248  # counted using sed -n '$=' NumObs.txt
 # Define a function to be applied to each chunk
 def process_chunk(chunk):
     chunk.columns = ['packed_desig', 'year', 'obs']
-    # get all rows where the last three digits are either G96 or 703 (the CSS observatories) and between 2005 and 2012
+    # get all rows where the last three digits are either G96 or 703 (the CSS observatories) and between 2005 and 2012 inclusive
     processed_chunk = chunk[((chunk.obs.astype('string').str[-3:] == 'G96') | (chunk.obs.astype('string').str[-3:] == '703')) &
-    ((chunk.year.astype('string').str[-4:].astype('int') > 2005) & (chunk.year.astype('string').str[-4:].astype('int') < 2012))]
+    ((chunk.year.astype('string').str[-4:].astype('int') >= 2005) & (chunk.year.astype('string').str[-4:].astype('int') <= 2012))]
     # write to csv
     processed_chunk.to_csv('mpc_data/mpc_processed_' + str(i) + '.csv')
+    print(processed_chunk.packed_desig.value_counts())
     return None
 
 # Read the large DataFrame in chunks
-chunks = pd.read_fwf('NumObs.txt', sep=None, chunksize=chunksize, header=None, usecols=[0, 1, 10], quoting=3, on_bad_lines='skip')
+chunks = pd.read_fwf('NumObs.txt', chunksize=chunksize, sep=None, header=None, usecols=[0, 1, 10], quoting=3, on_bad_lines='skip')
 
-# # pool
-# pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)  # avoids locking up the CPU by using every core
-# result = tqdm(pool.imap(process_chunk, zip(enumerate(chunks))))
-
-for i, chunk in tqdm(enumerate(chunks), total=int(n_lines/chunksize)):  # approximate total number of iterations
+for i, chunk in tqdm(enumerate(chunks)):  # approximate total number of iterations
     process_chunk(chunk)  # process chunk
